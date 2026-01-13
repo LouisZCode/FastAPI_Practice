8a63a595d60657010391673fb80594f827822baf
Luis Zermeno <lgzg90@hotmail.com>
2026-01-13 17:50:19 +0100

    update: successfully finish a new CRUD mini exercise

---
+++ exercise_GET_POST/put_delete.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+#uvicorn put_delete:app --reload
+
+app = FastAPI()
+
+notes_db = {}
+
+class  NoteBody(BaseModel):
+    title: str
+    content : str
+    category : str
+
+@app.post("/notes/")
+async def create_note( data : NoteBody):
+    note_id = len(notes_db) +1
+    notes_db[note_id] = {"title" : data.title, "content" : data.content, "category" : data.category}
+    return notes_db[note_id]
+
+
+@app.get("/notes/")
+async def get_all_notes(category : str = None):
+    if category == None:
+        return notes_db
+
+    selected_notes = []
+    for note_id , note in notes_db.items():
+        if note["category"] == category:
+            selected_notes.append(note)
+    
+    return selected_notes
+
+
+@app.get("/notes/{note_id}")
+async def get_note(note_id : int):
+    if note_id not in notes_db:
+        raise HTTPException(status_code=404, detail=f"No note with id: {note_id}")
+    
+    return notes_db[note_id]
+
+@app.put("/notes/{note_id}")
+async def edit_note(note_id : int, data : NoteBody):
+    if note_id not in notes_db:
+        raise HTTPException(status_code=404, detail=f"No note with id: {note_id}")
+    
+    notes_db[note_id] = {"title" : data.title, "content" : data.content, "category" : data.category}
+    return notes_db[note_id]
+
+
+@app.delete("/notes/{note_id}")
+async def delete_note(note_id : int):
+    if note_id not in notes_db:
+        raise HTTPException(status_code=404, detail=f"No note with id: {note_id}")
+    
+    deleted_note = notes_db.pop(note_id)
+    return deleted_note
+
