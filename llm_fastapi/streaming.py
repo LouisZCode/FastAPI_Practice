7741fd273f60df045b9054486096bfff1181164a
Luis Zermeno <lgzg90@hotmail.com>
2026-01-14 14:46:19 +0100

    update: success streaming an answer using FastAPI and Async - Await - Astream form Langchain

--- llm_fastapi/streaming.py
+++ llm_fastapi/streaming.py
@@ -1,1 +1,21 @@
 # uvicorn streaming:app --reload
+from simple_agent import agent
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class UserMessage(BaseModel):
+    message : str = Field(min_length=1)
+
+@app.post("/chat/")
+async def call_agent(user_message: UserMessage):
+
+    async def generate():
+        async for chunk in agent.astream({"messages": {"role": "user", "content": user_message.message}}, stream_mode="messages"):
+            msg, metadata = chunk  # Unpack tuple
+            if msg.content:  # Only yield if there's content
+                yield msg.content
+    
+    return StreamingResponse(generate(), media_type="text/plain")
