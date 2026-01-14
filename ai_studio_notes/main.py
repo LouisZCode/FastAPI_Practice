39ca7eb64cfec839e1e314b3475517633a2e5d4b
Luis Zermeno <lgzg90@hotmail.com>
2026-01-14 17:29:44 +0100

    update: initialize capstone mini project with LLM, routing, CRUD, Errir Handling, filtering, etc..

---
+++ ai_studio_notes/main.py
@@ -0,0 +1,56 @@
+#   uvicorn main:app --reload
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel, Field
+from typing import Literal
+
+notes_db = {}
+app = FastAPI()
+
+def note_or_404(note_id : int):
+    if note_id not in notes_db:
+        raise HTTPException(status_code=404, detail="No note with htis id exists")
+    return notes_db[note_id]
+
+
+class NoteBody(BaseModel):
+    title : str = Field(min_length=1, max_length=1000)
+    content : str
+    subject : Literal["math", "history", "science"]
+
+@app.post("/notes/")
+async def create_note(note : NoteBody):
+    id = len(notes_db) +1
+    new_note = {"title" : note.title, "content": note.content, "subject" : note.subject}
+    notes_db[id] = new_note
+
+    return {"new_note" : new_note}
+
+@app.get("/notes/")
+async def read_all_notes(subject : str = None):
+    if subject is None:
+        return notes_db
+    
+    filtered = []
+    for id , note in notes_db.items():
+        if note.subject == subject:
+            filtered.append(note)
+    
+    return filtered
+
+
+
+@app.get("/notes/{note_id}")
+async def get_note_by_id(note : dict = Depends(note_or_404)):
+    return note
+
+
+@app.put("/notes/{note_id}")
+async def edit_note(note_id : int, data : NoteBody, note : dict = Depends(note_or_404)):
+    notes_db[note_id] = {"title" : data.title, "content": data.content, "subject" : data.subject}
+    return notes_db[note_id]
+
+
+@app.delete("/notes/{note_id}")
+async def delete_note(note_id : int, note : dict = Depends(note_or_404)):
+    return {"deleted" : notes_db.pop(note_id)}
+
