47be56363bd7c2ebe5a7b219852df7428a34485b
Luis Zermeno <lgzg90@hotmail.com>
2026-01-17 10:27:18 +0100

    feature: get all topics or filter by category

--- ai_flashcard/routes/topics.py
+++ ai_flashcard/routes/topics.py
@@ -1,17 +1,32 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from langchain_core.tools import retriever
+from pydantic import BaseModel, Field
+from typing import Literal
 
 topics_db = {}
 
 router = APIRouter()
 
 class TopicBody(BaseModel):
-    name : str
+    name : str = Field(min_length=2, max_length=20)
     category : str
 
 @router.post("/topics/")
-def create_topic(topic : TopicBody):
+async def create_topic(topic : TopicBody):
     topic_id = len(topics_db) + 1
     topic_content = {"name" : topic.name, "category" : topic.category}
     topics_db[topic_id] = topic_content
     return topics_db[topic_id]
+
+@router.get("/topics/")
+async def list_topics(category : str = None):
+    if category is None:
+        return topics_db
+
+    filtered_topics = []
+    for id, topic in topics_db.items():
+        if category == topic["category"]:
+            filtered_topics.append(topic)
+
+    return filtered_topics
+    
