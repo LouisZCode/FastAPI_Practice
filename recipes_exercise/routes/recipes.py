e4372ad92b5309e7d6e91acb9b93fa35d3461fd1
Luis Zermeno <lgzg90@hotmail.com>
2026-01-19 14:26:14 +0100

    feature: add of a request limit to my LLM API Call

--- recipes_exercise/routes/recipes.py
+++ recipes_exercise/routes/recipes.py
@@ -11,6 +11,9 @@ api_header = APIKeyHeader(name="X-API-KEY")
 recipes_db = {}
 improvements_db = {}
 
+LLM_CALL_RATE_LIMIT = 2
+usage_counts = {}
+
 valid_api_keys = {
     "api_key_123" : "junior_dev_01",
     "api_key_456" : "senior_dev_02"
@@ -33,6 +36,16 @@ def validate_recipe_id(recipe_id : int):
         raise  HTTPException(status_code=404, detail=f"No such recipe with id {recipe_id} in database")
     else:
         return recipes_db[recipe_id]
+
+def validate_in_rate_call(api_key : dict = Depends(validate_api_key)):
+    current_count = usage_counts.get(api_key, 0)
+
+    if current_count >= LLM_CALL_RATE_LIMIT:
+        raise HTTPException(status_code=429, detail="Too many requests, try again in 24 hours")
+
+    usage_counts[api_key] = current_count + 1
+
+    return api_key
 
 @router.post("/recipes/")
 async def create_recipe(recipe : RecipeBody, api_key : str = Depends(validate_api_key)):
@@ -63,7 +76,7 @@ async def delete_recipe(recipe_id : int, recipe : dict = Depends(validate_recipe
     return {"deleted_recipe" : recipes_db.pop(recipe_id)}
 
 @router.post("/recipes/{recipe_id}")
-async def ai_improve_recipe(recipe_id : int, recipe : dict = Depends(validate_recipe_id), api_key : str = Depends(validate_api_key)):
+async def ai_improve_recipe(recipe_id : int, recipe : dict = Depends(validate_recipe_id), api_key : str = Depends(validate_in_rate_call)):
     answer = await recipe_agent.ainvoke({"messages" : {"role" : "user", "content" : f"The user sends this recipe: {recipe}"}})
     improvements_db[recipe_id] = answer["messages"][-1].content
     return {"llm_answer" : improvements_db[recipe_id]}
