4f5e6989bff6ebdccecea82223adee921a702865
Luis Zermeno <lgzg90@hotmail.com>
2026-01-19 14:32:33 +0100

    feature: add of logs Middleware for http calls

--- recipes_exercise/main.py
+++ recipes_exercise/main.py
@@ -1,7 +1,8 @@
 #   uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes import router as recipes_router
+import time
 
 app = FastAPI()
 app.include_router(recipes_router)
@@ -10,3 +11,12 @@ app.include_router(recipes_router)
 async def check_health():
     return {"status" : "ok"}
 
+@app.middleware("/http/")
+async def log_info(request : Request, call_next):
+    start = time.time()
+
+    response = await call_next(request)
+
+    duration = time.time() - start
+    print(f"{request.method}, {request.url.path} - {duration:.2f}s")
+    return response
