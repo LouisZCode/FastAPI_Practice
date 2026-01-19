702e978a2e7d4adf136067a2268b810502742b8a
Luis Zermeno <lgzg90@hotmail.com>
2026-01-19 10:47:18 +0100

    feature: get specific recipe by ID with exception raises

--- recipes_exercise/main.py
+++ recipes_exercise/main.py
@@ -1,6 +1,6 @@
 #   uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel, Field
 from typing import Literal
 
@@ -14,6 +14,12 @@ class RecipeBody(BaseModel):
     cuisine : Literal["mexican", "italian", "japanese"]
     ingredients : list = Field(min_length=3)
     instructions : str
+
+def validate_recipe_id(recipe_id : int):
+    if recipe_id not in recipes_db:
+        raise  HTTPException(status_code=404, detail=f"No such recipe with id {recipe_id} in database")
+    else:
+        return recipes_db[recipe_id]
 
 @app.post("/recipes/")
 async def create_recipe(recipe : RecipeBody):
@@ -33,3 +39,8 @@ async def get_all_recipes(cuisine : str = None):
                 filtered_recipes.append(recipe)
         
         return filtered_recipes
+
+
+@app.get("/recipes/{recipe_id}")
+async def get_one_recipe(recipe : dict = Depends(validate_recipe_id)):
+    return recipe
